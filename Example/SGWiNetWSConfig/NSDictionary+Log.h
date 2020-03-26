91d9ee27eed2fdc3d365fd66dc96e02dc7de98fe
yuqiang <1054572107@qq.com>
2020-03-26 23:33:49 +0800

    unicode 解析

---
+++ Example/SGWiNetWSConfig/NSDictionary+Log.h
@@ -0,0 +1,17 @@
+//
+//  NSDictionary+Log.h
+//  SGWiNetWSConfig_Example
+//
+//  Created by sungrow on 2020/3/26.
+//  Copyright © 2020 1054572107@qq.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSDictionary (Log)
+
+@end
+
+NS_ASSUME_NONNULL_END
